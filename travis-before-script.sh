e7841f7087f6d1fe16ab4e1e86e937a2152e4065
Anh Cao <5773176+anhcao142@users.noreply.github.com>
2019-11-21 21:43:44 +0700

    Create travis-before-script.sh

---
+++ travis-before-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Download and extract wabt
+wget -c https://github.com/WebAssembly/wabt/releases/download/1.0.12/wabt-1.0.12-linux.tar.gz -O - | tar -xz
+
+# Add wat2wasm to path
+export PATH=$PATH:$(pwd)/wabt-1.0.12
