59d6802517c9c9e39d105fcd001b1d5d42ec10c5
Anh Cao <5773176+anhcao142@users.noreply.github.com>
2019-11-22 15:59:33 +0700

    Update travis-before-script.sh

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Download golangci-lint
 wget -O - -q https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.21.0
-golangci-lint --version
+
+# Move golangci-lint to path
+sudo mv ./bin/* /usr/local/bin/
 
 # Download and extract wabt
 wget -c https://github.com/WebAssembly/wabt/releases/download/1.0.12/wabt-1.0.12-linux.tar.gz -O - | tar -xz && ls
